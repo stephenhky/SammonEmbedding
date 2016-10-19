799208645d0fb5daa740b20677a6a589c300d391
stephenhky <cwin31>
2016-10-18 23:49:57 -0400

    up to E, things have been correct. but gradE is outputting nan

--- sammon.py
+++ sammon.py
@@ -14,23 +14,26 @@ Ymatrix = T.dmatrix('Ymatrix')
 N, d = Xmatrix.shape
 
 # distance function (Euclidean distance)
-dist = lambda i, j: T.sqrt(T.sqr(Xmatrix[i]-Xmatrix[j]))
-tdist = lambda i, j: T.sqrt(T.sqr(Ymatrix[i]-Ymatrix[j]))
+dist = lambda i, j: T.sqrt(T.sum(T.sqr(Xmatrix[i]-Xmatrix[j])))
+tdist = lambda i, j: T.sqrt(T.sum(T.sqr(Ymatrix[i]-Ymatrix[j])))
 
 # cost function
-c = T.sum(theano.map(lambda j: T.sum(theano.map(lambda i: dist(i, j), T.arange(j))[0]), T.arange(N))[0])
-E = T.sum(theano.map(lambda j: T.sum(theano.map(lambda i: T.sqr(dist(i, j)-tdist(i, j))/dist(i,j),
-                                                T.arange(j))[0]),
+c = T.sum(theano.map(lambda j: T.sum(theano.map(lambda i: T.switch(T.lt(i, j), dist(i, j), 0),
+                                                T.arange(N))[0]
+                                     ),
+                     T.arange(N))[0]
+          )
+E = T.sum(theano.map(lambda j: T.sum(theano.map(lambda i: T.switch(T.lt(i, j), T.sqr(dist(i, j)-tdist(i, j))/dist(i,j), 0),
+                                                T.arange(N))[0]
+                                     ),
                      T.arange(N))[0]
           )
 E = E / c
 
-# Efcn = theano.function([Xmatrix, Ymatrix], E)
-
 # gradient and second derivatives (not Hessian matrix)
 gradE = T.grad(E, Ymatrix)
 divgradE = theano.map(lambda i, j: T.grad(gradE[i, j], Ymatrix[i, j]), T.arange(N), T.arange(td))
-
-# update routine
+#
+# # update routine
 updated_Ymatrix = Ymatrix - mf * gradE / divgradE
 updatefcn = theano.funcion([Xmatrix, Ymatrix, mf], updated_Ymatrix)
