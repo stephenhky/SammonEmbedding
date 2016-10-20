3d436c01958c119d0aa1cc92041b3aed4de43cc7
stephenhky <stephenhky@yahoo.com.hk>
2016-10-20 12:20:11 -0400

    dealt with second derivative, yet to settle nan problem of gradient

--- sammon.py
+++ sammon.py
@@ -36,10 +36,19 @@ s = T.sum(theano.map(lambda j: T.sum(theano.map(lambda i: T.switch(T.lt(i, j),
           )
 E = s / c
 
-# gradient and second derivatives (not Hessian matrix)
+# gradient
 gradE = T.grad(E, Ymatrix)
-divgradE, _ = theano.map(lambda i, j: T.grad(gradE[i, j], Ymatrix[i, j]), T.arange(N), T.arange(td))
 
-# # update routine
+# second derivatives (not Hessian matrix)
+imgrid = T.mgrid[0:N, 0:td]
+flattenii = imgrid[0].flatten()
+flattenjj = imgrid[1].flatten()
+# divgradE, _ = theano.map(lambda i, j: T.grad(gradE[i, j], Ymatrix)[i, j], flattenii, flattenjj)
+divgradE, divgradupdates = theano.scan(lambda i, j, gE, Y: T.grad(gE[i, j], Y)[i, j],
+                                       sequences=[flattenii, flattenjj],
+                                       non_sequences=[gradE, Ymatrix])
+divgradE = T.reshape(divgradE, (N, td))
+
+# update routine
 updated_Ymatrix = Ymatrix - mf * gradE / divgradE
 updatefcn = theano.funcion([Xmatrix, Ymatrix, mf], updated_Ymatrix)
