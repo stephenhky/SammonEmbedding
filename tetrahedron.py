9c47989f18e4fe1f2f114094cd00a515101533bb
stephenhky <stephenhky@yahoo.com.hk>
2016-10-21 12:18:29 -0400

    tetrahedron embedding code stable; a figure of embedded tetrahedron attached.

--- tetrahedron.py
+++ tetrahedron.py
@@ -8,11 +8,14 @@ tetrahedron_points = [np.array([0., 0., 0.]),
                       np.array([np.cos(np.pi/3), np.sin(np.pi/3), 0.]),
                       np.array([0.5, 0.5/np.sqrt(3), np.sqrt(2./3.)])]
 
-sampled_points = np.concatenate([np.random.multivariate_normal(point, np.eye(3), 10)
+sampled_points = np.concatenate([np.random.multivariate_normal(point, np.eye(3)*0.0001, 10)
                                  for point in tetrahedron_points])
 
-init_points = np.concatenate([np.random.multivariate_normal(point[:2], np.eye(2), 10)
+init_points = np.concatenate([np.random.multivariate_normal(point[:2], np.eye(2)*0.0001, 10)
                               for point in tetrahedron_points])
 
-embed_points = sn.sammon_embedding(sampled_points, init_points)
-plt.plot(embed_points)
+embed_points = sn.sammon_embedding(sampled_points, init_points, tol=1e-4)
+
+X, Y = embed_points.transpose()
+plt.plot(X, Y)
+plt.savefig('embedded_tetrahedron.png')
