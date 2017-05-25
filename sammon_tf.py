ae46141306db0d4ebf4b8dfdd3818b4e58c421c2
stephenhky <stephenhky@yahoo.com.hk>
2017-05-25 12:13:38 -0400

    tensorflow implementation of Sammon Embedding

--- sammon_tf.py
+++ sammon_tf.py
@@ -1,37 +1,56 @@
-import numpy as np
+# import numpy as np
 import tensorflow as tf
 
-def sammon_embedding(Xmat, initYmat, alpha=0.3, nbsteps=500):
+def sammon_embedding(Xmat, initYmat, tol=1e-8, alpha=0.3, nbsteps=1000):
     N = Xmat.shape[0]
-    d = Xmat.shape[1]
-
-    # define X
-    X = tf.placeholder('float')
-    Xshape = tf.shape(X)
+    # d = Xmat.shape[1]
 
     # distance matrix for X
+    X = tf.placeholder('float')
     sqX = tf.reduce_sum(X * X, 1)
     sqX = tf.reshape(sqX, [-1, 1])
     sqDX = sqX - 2 * tf.matmul(X, tf.transpose(X)) + tf.transpose(sqX)
-    DX = tf.sqrt(sqDX)
+    sqDXarray = tf.stack([sqDX[i, j] for i in range(N) for j in range(i + 1, N)])
+    DXarray = tf.sqrt(sqDXarray)
 
     # distance matrix for Y
     Y = tf.Variable(initYmat, dtype='float')
     sqY = tf.reduce_sum(Y * Y, 1)
     sqY = tf.reshape(sqY, [-1, 1])
     sqDY = sqY - 2 * tf.matmul(Y, tf.transpose(Y)) + tf.transpose(sqY)
-    DY = tf.sqrt(sqDY)
+    sqDYarray = tf.stack([sqDY[i, j] for i in range(N) for j in range(i + 1, N)])
+    DYarray = tf.sqrt(sqDYarray)
 
     # cost function
-    Z = tf.reduce_sum(DX) * 0.5
-    numerator = tf.reduce_sum(tf.divide(tf.square(DX - DY), DX + tf.diag(tf.ones(N)))) * 0.5
-    cost = numerator / Z
+    Z = tf.reduce_sum(DXarray) * 0.5
+    numerator = tf.reduce_sum(tf.divide(tf.square(DXarray - DYarray), DXarray)) * 0.5
+    cost = tf.divide(numerator, Z)
 
-    # optimizer
-    optimizer = tf.train.GradientDescentOptimizer(alpha).minimize(cost)
+    # optimizer (note: do not use Adam, which can lead to overflow)
+    train = tf.train.AdagradOptimizer(alpha).minimize(cost)
     init = tf.global_variables_initializer()
 
     # Tensorflow session
     sess = tf.Session()
     sess.run(init)
-    embedded_pts = sess.run()
+
+    # training
+    c = sess.run(cost, feed_dict={X: Xmat})
+    print "initial cost = ", c
+    converged = False
+    i = 0
+    while (not converged) and (i < nbsteps):
+        sess.run(train, feed_dict={X: Xmat})
+        newc = sess.run(cost, feed_dict={X: Xmat})
+        print "epoch: ", i, " cost = ", newc
+        converged = (abs(newc-c)<tol)
+        i += 1
+        c = newc
+
+    # result retrieval
+    calculated_Y = sess.run(Y, feed_dict={X: Xmat})
+
+    # close session
+    sess.close()
+
+    return calculated_Y
--- tetrahedron.py
+++ tetrahedron.py
@@ -4,8 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import sammon as sn
+import sammon_tf as sntf
 
 argparser = argparse.ArgumentParser('Embedding points around tetrahedron.')
+argparser.add_argument('framework', help='framework (theano or tensorflow)')
 argparser.add_argument('--output_figurename',
                        default='embedded_tetrahedron.png',
                        help='file name of the output plot')
@@ -23,7 +25,10 @@ sampled_points = np.concatenate([np.random.multivariate_normal(point, np.eye(3)*
 init_points = np.concatenate([np.random.multivariate_normal(point[:2], np.eye(2)*0.0001, 10)
                               for point in tetrahedron_points])
 
-embed_points = sn.sammon_embedding(sampled_points, init_points, tol=1e-4)
+if args.framework == 'theano':
+    embed_points = sn.sammon_embedding(sampled_points, init_points, tol=1e-4)
+else:
+    embed_points = sntf.sammon_embedding(sampled_points, init_points, tol=1e-4)
 
 X, Y = embed_points.transpose()
 plt.plot(X, Y, 'x')
